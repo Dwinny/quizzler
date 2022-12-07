003b9dff11051c3730c9dc991ba4f338f61ffc04
chioma <chiomag58@gmail.com>
2022-12-07 12:50:06 +0100

    init

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+// ignore: empty_constructor_bodies
+// ignore_for_file: empty_constructor_bodies
+
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question(this.questionText, this.questionAnswer);
+}
